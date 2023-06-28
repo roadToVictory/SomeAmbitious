5db3611954dca5dccbdedaa6b292d83272973376
koperstynski <koperstynski@student.agh.edu.pl>
2023-06-28 21:31:38 +0200

    initial

---
+++ src/main/java/com/example/SomeAmbitious/AmbitiousApplication.java
@@ -0,0 +1,13 @@
+package com.example.SomeAmbitious;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbitiousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbitiousApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/SomeAmbitious/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.SomeAmbitious.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class HelloController {
+
+    @GetMapping
+    public String sayHello() {
+        log.info("jestem tu");
+        return "Hello mordo!";
+    }
+}
